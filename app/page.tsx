52145f9b6fe8aba1fc47db316ad4a13c4ec218b1
Yash sharma <yashsharmaofficially@gmail.com>
2026-02-02 17:18:13 +0530

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "@/components/Landing/header";
+import Footer from "@/components/Landing/footer";
 
 export default function Home() {
   return (
 
     <div>
-      <Header></Header>
+     
+      
       </div>
   )
 }
